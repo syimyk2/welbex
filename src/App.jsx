4f1b1bef81e1ec00f9604d203bc70d6a6da83e40
syimyk2 <syimyk.jetibaev99@gmail.com>
2023-05-05 00:57:01 +0600

    init: set up and configure project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+	return (
+		<div className='bg-slate-500 w-full'>
+			<h1 className='text-sm py-10 font-bold'>Bissmillah</h1>
+			<p>hello vite</p>
+		</div>
+	)
+}
+
+export default App
